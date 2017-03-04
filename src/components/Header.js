30e0ec357faab9eedfae5f5a103e556a5b80386f
Damian Marek <damianmateuszmarek@gmail.com>
2017-03-04 03:15:15 +0100

    Change header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles/Header.css'
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div className="header">
+        <h2 className="header-text">
+          Weatherify
+        </h2>
+      </div>
+    )
+  }
+}
+
+export default Header
