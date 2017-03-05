e5ef0732425bf100c306ef93e80dca8cadfad922
Damian Marek <damianmateuszmarek@gmail.com>
2017-03-05 17:55:32 +0100

    Add simple weather API system

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Weather from './components/Weather'
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      city: '',
+      degrees: 0,
+    }
+  }
+
+  handleButtonPress = (event) => {
+    this.setState({ city: this.textInput.value })
+    this.fetchWeather(this.textInput.value)
+  }
+
+  fetchWeather = (city) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=76deb9243afb0cb5277a40b4f8b77bf8&units=metric`)
+    .then(res => res.json())
+    .then((res) => {
+      this.setState({
+        degrees: res.main.temp
+      })
+    })
+    .catch(error => {
+      this.setState({
+        degrees: 0
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Show a temperature in your city
         </p>
+        <input ref={(input) => { this.textInput = input }} />
+        <button onClick={() => {this.handleButtonPress()}} >
+          Show
+        </button>
+        <Weather degrees={this.state.degrees} />
       </div>
     );
   }
